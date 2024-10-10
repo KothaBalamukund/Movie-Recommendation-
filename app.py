4086922ae68b84efbf6297cd7e2eb3089a5aae6b
KothaBalamukund <balurock6789@gmail.com>
2024-10-10 10:28:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,11 @@
 import streamlit as st
 import pickle
-import requests
-
-def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
 
 movies = pickle.load(open("movies_list.pkl", 'rb'))
 similarity = pickle.load(open("similarity.pkl", 'rb'))
 movies_list=movies['title'].values
 
 st.header("Movie Recommender System")
-
-import streamlit.components.v1 as components
-
-imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
-
-
-imageUrls = [
-    fetch_poster(1632),
-    fetch_poster(299536),
-    fetch_poster(17455),
-    fetch_poster(2830),
-    fetch_poster(429422),
-    fetch_poster(9722),
-    fetch_poster(13972),
-    fetch_poster(240),
-    fetch_poster(155),
-    fetch_poster(598),
-    fetch_poster(914),
-    fetch_poster(255709),
-    fetch_poster(572154)
-   
-    ]
-
-
-imageCarouselComponent(imageUrls=imageUrls, height=200)
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
 
 def recommend(movie):
     index=movies[movies['title']==movie].index[0]
